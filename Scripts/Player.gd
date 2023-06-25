b6d4102e67c2d04084d5b39e9d0015c3e8efd111
Gcosta <mailgc99@gmail.com>
2023-06-25 13:39:16 +0100

    Smooth movement Finalized

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,32 +1,32 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const FRICTION = 600.0
-const ACCELERATION = 1500.0
+const SPEED = 400.0
+const FRICTION = 1000.0
+const ACCELERATION = 1900.0
 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-
-func player_motion():
-	var directionRL = Input.get_axis("ui_left", "ui_right")
-	var directionUD = Input.get_axis("ui_up","ui_down")
-
-	if directionRL:
-		velocity.x = directionRL * SPEED 
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED/(1/FRICTION))
-	if directionUD:
-		velocity.y = directionUD * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	return velocity
+var input = Vector2.ZERO
 
 func _physics_process(delta):
+	player_motion(delta)
 	
-	player_motion()
+func get_input():	
+	input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	return input.normalized()
 
+func player_motion(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (delta*FRICTION):
+			velocity -= velocity.normalized() * (delta*FRICTION)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (input * ACCELERATION * delta)
+		velocity = velocity.limit_length(SPEED)
+	
 	move_and_slide()
+		
+	
